830a88075172590a209dbbcd5f4efa3bb7ca1020
zxthah <zxthah@qq.com>
2017-07-28 22:01:49 +0800

    add log

--- src/main/java/com/juvenxu/mvnbook/helloworld/HelloWorld.java
+++ src/main/java/com/juvenxu/mvnbook/helloworld/HelloWorld.java
@@ -1,5 +1,7 @@
 package com.juvenxu.mvnbook.helloworld;
 
+import org.apache.log4j.Logger;
+
 public class HelloWorld {
 
 	public String sayHello()
@@ -10,5 +12,7 @@ public class HelloWorld {
 	public static void main(String[] args)
 	{
 		System.out.print( new HelloWorld().sayHello() );
+        Logger logger = Logger.getLogger(HelloWorld.class);
+        logger.info("¿ªÊ¼");
 	}
 }
